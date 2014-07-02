b0ee8bd75591b87f0650ac956b772ca9e9735a8f
witchi <phosco@gmx.de>
2014-07-03 00:00:48 +0200

    ADD: some Override annotations.

--- com/gallery/GalleryRemote/prefs/ProxyPanel.java
+++ com/gallery/GalleryRemote/prefs/ProxyPanel.java
@@ -15,10 +15,10 @@ import javax.swing.UIManager;
 import com.gallery.GalleryRemote.util.GRI18n;
 
 /**
- * Created by IntelliJ IDEA. User: paour Date: May 8, 2003
+ * @author paour
+ * @version May 8, 2003
  */
-public class ProxyPanel extends PreferencePanel implements ActionListener,
-		PreferenceNames {
+public class ProxyPanel extends PreferencePanel implements ActionListener, PreferenceNames {
 	private static final long serialVersionUID = -4658845287207998568L;
 	public static final String MODULE = "ProxyPa";
 
@@ -35,10 +35,12 @@ public class ProxyPanel extends PreferencePanel implements ActionListener,
 	JTextField jProxyPassword = new JTextField();
 	JPanel jPanel1 = new JPanel();
 
+	@Override
 	public JLabel getIcon() {
 		return icon;
 	}
 
+	@Override
 	public void readProperties(PropertiesFile props) {
 		jUseProxy.setSelected(props.getBooleanProperty(USE_PROXY));
 
@@ -50,6 +52,7 @@ public class ProxyPanel extends PreferencePanel implements ActionListener,
 		resetUIState();
 	}
 
+	@Override
 	public void writeProperties(PropertiesFile props) {
 		props.setBooleanProperty(USE_PROXY, jUseProxy.isSelected());
 
@@ -65,30 +68,23 @@ public class ProxyPanel extends PreferencePanel implements ActionListener,
 			jProxyPort.setEnabled(true);
 			jProxyUsername.setEnabled(true);
 			jProxyPassword.setEnabled(true);
-			jProxyHost
-					.setBackground(UIManager.getColor("TextField.background"));
-			jProxyPort
-					.setBackground(UIManager.getColor("TextField.background"));
-			jProxyUsername.setBackground(UIManager
-					.getColor("TextField.background"));
-			jProxyPassword.setBackground(UIManager
-					.getColor("TextField.background"));
+			jProxyHost.setBackground(UIManager.getColor("TextField.background"));
+			jProxyPort.setBackground(UIManager.getColor("TextField.background"));
+			jProxyUsername.setBackground(UIManager.getColor("TextField.background"));
+			jProxyPassword.setBackground(UIManager.getColor("TextField.background"));
 		} else {
 			jProxyHost.setEnabled(false);
 			jProxyPort.setEnabled(false);
 			jProxyUsername.setEnabled(false);
 			jProxyPassword.setEnabled(false);
-			jProxyHost.setBackground(UIManager
-					.getColor("TextField.inactiveBackground"));
-			jProxyPort.setBackground(UIManager
-					.getColor("TextField.inactiveBackground"));
-			jProxyUsername.setBackground(UIManager
-					.getColor("TextField.inactiveBackground"));
-			jProxyPassword.setBackground(UIManager
-					.getColor("TextField.inactiveBackground"));
+			jProxyHost.setBackground(UIManager.getColor("TextField.inactiveBackground"));
+			jProxyPort.setBackground(UIManager.getColor("TextField.inactiveBackground"));
+			jProxyUsername.setBackground(UIManager.getColor("TextField.inactiveBackground"));
+			jProxyPassword.setBackground(UIManager.getColor("TextField.inactiveBackground"));
 		}
 	}
 
+	@Override
 	public void buildUI() {
 		jbInit();
 	}
@@ -104,40 +100,31 @@ public class ProxyPanel extends PreferencePanel implements ActionListener,
 		jProxyPort.setText("");
 		jProxyUsername.setText("");
 		jProxyPassword.setText("");
-		this.add(jUseProxy, new GridBagConstraints(0, 0, 2, 1, 0.0, 0.0,
-				GridBagConstraints.WEST, GridBagConstraints.NONE, new Insets(0,
-						0, 0, 0), 0, 0));
-		this.add(jLabel1, new GridBagConstraints(0, 1, 1, 1, 0.0, 0.0,
-				GridBagConstraints.WEST, GridBagConstraints.NONE, new Insets(0,
-						20, 5, 5), 0, 0));
-		this.add(jLabel2, new GridBagConstraints(0, 2, 1, 1, 0.0, 0.0,
-				GridBagConstraints.WEST, GridBagConstraints.NONE, new Insets(0,
-						20, 5, 5), 0, 0));
-		this.add(jLabel3, new GridBagConstraints(0, 3, 1, 1, 0.0, 0.0,
-				GridBagConstraints.WEST, GridBagConstraints.NONE, new Insets(0,
-						20, 5, 5), 0, 0));
-		this.add(jLabel4, new GridBagConstraints(0, 4, 1, 1, 0.0, 0.0,
-				GridBagConstraints.WEST, GridBagConstraints.NONE, new Insets(0,
-						20, 5, 5), 0, 0));
-		this.add(jProxyHost, new GridBagConstraints(1, 1, 1, 1, 1.0, 0.0,
-				GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL,
+		this.add(jUseProxy, new GridBagConstraints(0, 0, 2, 1, 0.0, 0.0, GridBagConstraints.WEST, GridBagConstraints.NONE, new Insets(0, 0,
+				0, 0), 0, 0));
+		this.add(jLabel1, new GridBagConstraints(0, 1, 1, 1, 0.0, 0.0, GridBagConstraints.WEST, GridBagConstraints.NONE, new Insets(0, 20, 5,
+				5), 0, 0));
+		this.add(jLabel2, new GridBagConstraints(0, 2, 1, 1, 0.0, 0.0, GridBagConstraints.WEST, GridBagConstraints.NONE, new Insets(0, 20, 5,
+				5), 0, 0));
+		this.add(jLabel3, new GridBagConstraints(0, 3, 1, 1, 0.0, 0.0, GridBagConstraints.WEST, GridBagConstraints.NONE, new Insets(0, 20, 5,
+				5), 0, 0));
+		this.add(jLabel4, new GridBagConstraints(0, 4, 1, 1, 0.0, 0.0, GridBagConstraints.WEST, GridBagConstraints.NONE, new Insets(0, 20, 5,
+				5), 0, 0));
+		this.add(jProxyHost, new GridBagConstraints(1, 1, 1, 1, 1.0, 0.0, GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL,
 				new Insets(0, 0, 5, 0), 0, 0));
-		this.add(jProxyPort, new GridBagConstraints(1, 2, 1, 1, 1.0, 0.0,
-				GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL,
+		this.add(jProxyPort, new GridBagConstraints(1, 2, 1, 1, 1.0, 0.0, GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL,
 				new Insets(0, 0, 5, 0), 0, 0));
-		this.add(jProxyUsername, new GridBagConstraints(1, 3, 1, 1, 1.0, 0.0,
-				GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL,
+		this.add(jProxyUsername, new GridBagConstraints(1, 3, 1, 1, 1.0, 0.0, GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL,
 				new Insets(0, 0, 5, 0), 0, 0));
-		this.add(jProxyPassword, new GridBagConstraints(1, 4, 1, 1, 1.0, 0.0,
-				GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL,
+		this.add(jProxyPassword, new GridBagConstraints(1, 4, 1, 1, 1.0, 0.0, GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL,
 				new Insets(0, 0, 5, 0), 0, 0));
-		this.add(jPanel1, new GridBagConstraints(0, 5, 2, 1, 1.0, 1.0,
-				GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(
-						0, 0, 0, 0), 0, 0));
+		this.add(jPanel1, new GridBagConstraints(0, 5, 2, 1, 1.0, 1.0, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(0, 0,
+				0, 0), 0, 0));
 
 		jUseProxy.addActionListener(this);
 	}
 
+	@Override
 	public void actionPerformed(ActionEvent e) {
 		resetUIState();
 	}

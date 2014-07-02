557b173bd210c3a8317c8ae29d7dd5e85ac9dc00
witchi <phosco@gmx.de>
2014-07-03 00:05:15 +0200

    ADD: a new implementation of hashcode()

--- com/gallery/GalleryRemote/prefs/SlideshowPanel.java
+++ com/gallery/GalleryRemote/prefs/SlideshowPanel.java
@@ -25,7 +25,8 @@ import com.gallery.GalleryRemote.util.GRI18n;
 import com.gallery.GalleryRemote.util.ImageUtils;
 
 /**
- * Created by IntelliJ IDEA. User: paour Date: May 8, 2003
+ * @author paour
+ * @version May 8, 2003
  */
 public class SlideshowPanel extends PreferencePanel implements PreferenceNames {
 	private static final long serialVersionUID = -7417829136364194239L;
@@ -66,25 +67,20 @@ public class SlideshowPanel extends PreferencePanel implements PreferenceNames {
 	ColorWellButton jBackgroundColor = new ColorWellButton(Color.red);
 	JTabbedPane tabs = new JTabbedPane();
 
+	@Override
 	public JLabel getIcon() {
 		return icon;
 	}
 
+	@Override
 	public void readProperties(PropertiesFile props) {
-		jProgress.setSelectedItem(new LocationItem(props
-				.getIntProperty(SLIDESHOW_PROGRESS)));
-		jCaption.setSelectedItem(new LocationItem(props
-				.getIntProperty(SLIDESHOW_CAPTION)));
-		jExtra.setSelectedItem(new LocationItem(props
-				.getIntProperty(SLIDESHOW_EXTRA)));
-		jUrl.setSelectedItem(new LocationItem(props
-				.getIntProperty(SLIDESHOW_URL)));
-		jAlbum.setSelectedItem(new LocationItem(props
-				.getIntProperty(SLIDESHOW_ALBUM)));
-		jSummary.setSelectedItem(new LocationItem(props
-				.getIntProperty(SLIDESHOW_SUMMARY)));
-		jDescription.setSelectedItem(new LocationItem(props
-				.getIntProperty(SLIDESHOW_DESCRIPTION)));
+		jProgress.setSelectedItem(new LocationItem(props.getIntProperty(SLIDESHOW_PROGRESS)));
+		jCaption.setSelectedItem(new LocationItem(props.getIntProperty(SLIDESHOW_CAPTION)));
+		jExtra.setSelectedItem(new LocationItem(props.getIntProperty(SLIDESHOW_EXTRA)));
+		jUrl.setSelectedItem(new LocationItem(props.getIntProperty(SLIDESHOW_URL)));
+		jAlbum.setSelectedItem(new LocationItem(props.getIntProperty(SLIDESHOW_ALBUM)));
+		jSummary.setSelectedItem(new LocationItem(props.getIntProperty(SLIDESHOW_SUMMARY)));
+		jDescription.setSelectedItem(new LocationItem(props.getIntProperty(SLIDESHOW_DESCRIPTION)));
 
 		jLowRez.setSelected(props.getBooleanProperty(SLIDESHOW_LOWREZ));
 		jRandom.setSelected(props.getBooleanProperty(SLIDESHOW_RANDOM));
@@ -92,8 +88,7 @@ public class SlideshowPanel extends PreferencePanel implements PreferenceNames {
 		jPreloadAll.setSelected(props.getBooleanProperty(SLIDESHOW_PRELOADALL));
 		jLoop.setSelected(props.getBooleanProperty(SLIDESHOW_LOOP));
 		jDelay.setValue(props.getIntProperty(SLIDESHOW_DELAY) * 1000);
-		jTransition.setValue(props
-				.getIntProperty(SLIDESHOW_TRANSITION_DURATION));
+		jTransition.setValue(props.getIntProperty(SLIDESHOW_TRANSITION_DURATION));
 		Color color = props.getColorProperty(SLIDESHOW_COLOR);
 		jOverride.setSelected(color != null);
 		jBackgroundColor.setSelectedColor(color);
@@ -111,33 +106,25 @@ public class SlideshowPanel extends PreferencePanel implements PreferenceNames {
 		jPreloadAll.setEnabled(!props.isOverridden(SLIDESHOW_PRELOADALL));
 		jLoop.setEnabled(!props.isOverridden(SLIDESHOW_LOOP));
 		jDelay.setEnabled(!props.isOverridden(SLIDESHOW_DELAY));
-		jTransition.setEnabled(!props
-				.isOverridden(SLIDESHOW_TRANSITION_DURATION));
+		jTransition.setEnabled(!props.isOverridden(SLIDESHOW_TRANSITION_DURATION));
 		jOverride.setEnabled(!props.isOverridden(SLIDESHOW_COLOR));
 		jBackgroundColor.setEnabled(!props.isOverridden(SLIDESHOW_COLOR));
 	}
 
+	@Override
 	public void writeProperties(PropertiesFile props) {
-		if (jNoStretch.isSelected() != props
-				.getBooleanProperty(SLIDESHOW_NOSTRETCH)) {
+		if (jNoStretch.isSelected() != props.getBooleanProperty(SLIDESHOW_NOSTRETCH)) {
 			ImageUtils.purgeTemp();
 			GalleryRemote.instance().getCore().flushMemory();
 		}
 
-		props.setIntProperty(SLIDESHOW_PROGRESS,
-				((LocationItem) jProgress.getSelectedItem()).id);
-		props.setIntProperty(SLIDESHOW_CAPTION,
-				((LocationItem) jCaption.getSelectedItem()).id);
-		props.setIntProperty(SLIDESHOW_EXTRA,
-				((LocationItem) jExtra.getSelectedItem()).id);
-		props.setIntProperty(SLIDESHOW_URL,
-				((LocationItem) jUrl.getSelectedItem()).id);
-		props.setIntProperty(SLIDESHOW_ALBUM,
-				((LocationItem) jAlbum.getSelectedItem()).id);
-		props.setIntProperty(SLIDESHOW_SUMMARY,
-				((LocationItem) jSummary.getSelectedItem()).id);
-		props.setIntProperty(SLIDESHOW_DESCRIPTION,
-				((LocationItem) jDescription.getSelectedItem()).id);
+		props.setIntProperty(SLIDESHOW_PROGRESS, ((LocationItem) jProgress.getSelectedItem()).id);
+		props.setIntProperty(SLIDESHOW_CAPTION, ((LocationItem) jCaption.getSelectedItem()).id);
+		props.setIntProperty(SLIDESHOW_EXTRA, ((LocationItem) jExtra.getSelectedItem()).id);
+		props.setIntProperty(SLIDESHOW_URL, ((LocationItem) jUrl.getSelectedItem()).id);
+		props.setIntProperty(SLIDESHOW_ALBUM, ((LocationItem) jAlbum.getSelectedItem()).id);
+		props.setIntProperty(SLIDESHOW_SUMMARY, ((LocationItem) jSummary.getSelectedItem()).id);
+		props.setIntProperty(SLIDESHOW_DESCRIPTION, ((LocationItem) jDescription.getSelectedItem()).id);
 
 		props.setBooleanProperty(SLIDESHOW_LOWREZ, jLowRez.isSelected());
 		props.setBooleanProperty(SLIDESHOW_RANDOM, jRandom.isSelected());
@@ -145,12 +132,10 @@ public class SlideshowPanel extends PreferencePanel implements PreferenceNames {
 		props.setBooleanProperty(SLIDESHOW_PRELOADALL, jPreloadAll.isSelected());
 		props.setBooleanProperty(SLIDESHOW_LOOP, jLoop.isSelected());
 		props.setIntProperty(SLIDESHOW_DELAY, jDelay.getValue() / 1000);
-		props.setIntProperty(SLIDESHOW_TRANSITION_DURATION,
-				jTransition.getValue());
+		props.setIntProperty(SLIDESHOW_TRANSITION_DURATION, jTransition.getValue());
 
 		if (jOverride.isSelected()) {
-			props.setColorProperty(SLIDESHOW_COLOR,
-					jBackgroundColor.getSelectedColor());
+			props.setColorProperty(SLIDESHOW_COLOR, jBackgroundColor.getSelectedColor());
 		} else {
 			props.setProperty(SLIDESHOW_COLOR, null);
 		}
@@ -160,6 +145,7 @@ public class SlideshowPanel extends PreferencePanel implements PreferenceNames {
 		}
 	}
 
+	@Override
 	public void buildUI() {
 		jbInit();
 	}
@@ -191,13 +177,11 @@ public class SlideshowPanel extends PreferencePanel implements PreferenceNames {
 		/*
 		 * progressionPanel.setBorder( new
 		 * TitledBorder(BorderFactory.createEtchedBorder(Color.white, new
-		 * Color(148, 145, 140)), GRI18n.getString(MODULE,
-		 * "progressionTitle")));
+		 * Color(148, 145, 140)), GRI18n.getString(MODULE, "progressionTitle")));
 		 */
 		locationPanel.setLayout(new GridBagLayout());
-		locationPanel.setBorder(new TitledBorder(BorderFactory
-				.createEtchedBorder(Color.white, new Color(148, 145, 140)),
-				GRI18n.getString(MODULE, "locationTitle")));
+		locationPanel.setBorder(new TitledBorder(BorderFactory.createEtchedBorder(Color.white, new Color(148, 145, 140)), GRI18n.getString(
+				MODULE, "locationTitle")));
 		apperancePanel.setLayout(new GridBagLayout());
 		/*
 		 * apperancePanel.setBorder( new
@@ -211,8 +195,7 @@ public class SlideshowPanel extends PreferencePanel implements PreferenceNames {
 		jDelay.setMinimum(0);
 		jDelay.setMaximum(30000);
 		Hashtable<Integer, JLabel> ticks = new Hashtable<Integer, JLabel>(4);
-		ticks.put(new Integer(0),
-				new JLabel(GRI18n.getString(MODULE, "delayNone")));
+		ticks.put(new Integer(0), new JLabel(GRI18n.getString(MODULE, "delayNone")));
 		ticks.put(new Integer(5000), new JLabel("5s"));
 		ticks.put(new Integer(10000), new JLabel("10s"));
 		ticks.put(new Integer(30000), new JLabel("30s"));
@@ -227,8 +210,7 @@ public class SlideshowPanel extends PreferencePanel implements PreferenceNames {
 		jTransition.setMinimum(0);
 		jTransition.setMaximum(5000);
 		ticks = new Hashtable<Integer, JLabel>(4);
-		ticks.put(new Integer(0),
-				new JLabel(GRI18n.getString(MODULE, "transitionNone")));
+		ticks.put(new Integer(0), new JLabel(GRI18n.getString(MODULE, "transitionNone")));
 		ticks.put(new Integer(1000), new JLabel("1s"));
 		ticks.put(new Integer(2000), new JLabel("2s"));
 		ticks.put(new Integer(5000), new JLabel("5s"));
@@ -273,16 +255,15 @@ public class SlideshowPanel extends PreferencePanel implements PreferenceNames {
 		jLowRez.setToolTipText(GRI18n.getString(MODULE, "lowRezHelp"));
 		jOverride.setText(GRI18n.getString(MODULE, "backgroundColor"));
 
-		this.add(tabs, new GridBagConstraints(0, 0, 1, 1, 1.0, 1.0,
-				GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(
-						0, 0, 0, 0), 0, 0));
+		this.add(tabs, new GridBagConstraints(0, 0, 1, 1, 1.0, 1.0, GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+				new Insets(0, 0, 0, 0), 0, 0));
 
 		tabs.add(GRI18n.getString(MODULE, "progressionTitle"), progressionPanel);
 		tabs.add(GRI18n.getString(MODULE, "appearanceTitle"), apperancePanel);
 
 		/*
-		 * this.add(progressionPanel, new GridBagConstraints(0, 0, 1, 1, 1.0,
-		 * 0.0 ,GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL, new
+		 * this.add(progressionPanel, new GridBagConstraints(0, 0, 1, 1, 1.0, 0.0
+		 * ,GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL, new
 		 * Insets(0, 0, 0, 0), 0, 0)); this.add(locationPanel, new
 		 * GridBagConstraints(0, 1, 1, 1, 1.0, 0.0 ,GridBagConstraints.CENTER,
 		 * GridBagConstraints.HORIZONTAL, new Insets(0, 0, 0, 0), 0, 0));
@@ -290,100 +271,69 @@ public class SlideshowPanel extends PreferencePanel implements PreferenceNames {
 		 * ,GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL, new
 		 * Insets(0, 0, 0, 0), 0, 0));
 		 */
-		this.add(help, new GridBagConstraints(0, 3, 2, 1, 0.0, 0.0,
-				GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL,
-				new Insets(5, 5, 5, 0), 0, 0));
-		this.add(spacerPanel, new GridBagConstraints(0,
-				GridBagConstraints.REMAINDER, 1, 1, 1.0, 1.0,
-				GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(
-						0, 0, 0, 0), 0, 0));
+		this.add(help, new GridBagConstraints(0, 3, 2, 1, 0.0, 0.0, GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL, new Insets(5,
+				5, 5, 0), 0, 0));
+		this.add(spacerPanel, new GridBagConstraints(0, GridBagConstraints.REMAINDER, 1, 1, 1.0, 1.0, GridBagConstraints.CENTER,
+				GridBagConstraints.BOTH, new Insets(0, 0, 0, 0), 0, 0));
 
-		progressionPanel.add(delay, new GridBagConstraints(0, 0, 1, 1, 0.0,
-				0.0, GridBagConstraints.EAST, GridBagConstraints.NONE,
+		progressionPanel.add(delay, new GridBagConstraints(0, 0, 1, 1, 0.0, 0.0, GridBagConstraints.EAST, GridBagConstraints.NONE,
 				new Insets(0, 0, 0, 5), 0, 0));
-		progressionPanel.add(jDelay, new GridBagConstraints(1, 0, 1, 1, 1.0,
-				0.0, GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL,
+		progressionPanel.add(jDelay, new GridBagConstraints(1, 0, 1, 1, 1.0, 0.0, GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL,
 				new Insets(0, 0, 0, 0), 0, 0));
-		progressionPanel.add(transition, new GridBagConstraints(0, 1, 1, 1,
-				0.0, 0.0, GridBagConstraints.EAST, GridBagConstraints.NONE,
+		progressionPanel.add(transition, new GridBagConstraints(0, 1, 1, 1, 0.0, 0.0, GridBagConstraints.EAST, GridBagConstraints.NONE,
 				new Insets(0, 0, 0, 5), 0, 0));
-		progressionPanel.add(jTransition, new GridBagConstraints(1, 1, 1, 1,
-				1.0, 0.0, GridBagConstraints.CENTER,
+		progressionPanel.add(jTransition, new GridBagConstraints(1, 1, 1, 1, 1.0, 0.0, GridBagConstraints.CENTER,
 				GridBagConstraints.HORIZONTAL, new Insets(0, 0, 0, 0), 0, 0));
-		progressionPanel.add(jRandom, new GridBagConstraints(0, 2, 2, 1, 0.0,
-				0.0, GridBagConstraints.WEST, GridBagConstraints.NONE,
+		progressionPanel.add(jRandom, new GridBagConstraints(0, 2, 2, 1, 0.0, 0.0, GridBagConstraints.WEST, GridBagConstraints.NONE,
 				new Insets(0, 0, 0, 0), 0, 0));
-		progressionPanel.add(jPreloadAll, new GridBagConstraints(0, 3, 2, 1,
-				0.0, 0.0, GridBagConstraints.WEST, GridBagConstraints.NONE,
+		progressionPanel.add(jPreloadAll, new GridBagConstraints(0, 3, 2, 1, 0.0, 0.0, GridBagConstraints.WEST, GridBagConstraints.NONE,
 				new Insets(0, 0, 0, 0), 0, 0));
-		progressionPanel.add(jLoop, new GridBagConstraints(0, 4, 2, 1, 0.0,
-				0.0, GridBagConstraints.WEST, GridBagConstraints.NONE,
+		progressionPanel.add(jLoop, new GridBagConstraints(0, 4, 2, 1, 0.0, 0.0, GridBagConstraints.WEST, GridBagConstraints.NONE,
 				new Insets(0, 0, 0, 0), 0, 0));
-		progressionPanel.add(new JPanel(), new GridBagConstraints(0, 5, 2, 1,
-				0.0, 1.0, GridBagConstraints.WEST, GridBagConstraints.BOTH,
+		progressionPanel.add(new JPanel(), new GridBagConstraints(0, 5, 2, 1, 0.0, 1.0, GridBagConstraints.WEST, GridBagConstraints.BOTH,
 				new Insets(0, 0, 0, 0), 0, 0));
 
-		apperancePanel.add(jLowRez, new GridBagConstraints(0, 0, 2, 1, 1.0,
-				0.0, GridBagConstraints.WEST, GridBagConstraints.HORIZONTAL,
+		apperancePanel.add(jLowRez, new GridBagConstraints(0, 0, 2, 1, 1.0, 0.0, GridBagConstraints.WEST, GridBagConstraints.HORIZONTAL,
 				new Insets(0, 0, 0, 0), 0, 0));
-		apperancePanel.add(jNoStretch, new GridBagConstraints(0, 1, 2, 1, 0.0,
-				0.0, GridBagConstraints.WEST, GridBagConstraints.HORIZONTAL,
+		apperancePanel.add(jNoStretch, new GridBagConstraints(0, 1, 2, 1, 0.0, 0.0, GridBagConstraints.WEST, GridBagConstraints.HORIZONTAL,
 				new Insets(0, 0, 0, 0), 0, 0));
-		apperancePanel.add(jOverride, new GridBagConstraints(0, 2, 1, 1, 0.0,
-				0.0, GridBagConstraints.WEST, GridBagConstraints.NONE,
+		apperancePanel.add(jOverride, new GridBagConstraints(0, 2, 1, 1, 0.0, 0.0, GridBagConstraints.WEST, GridBagConstraints.NONE,
 				new Insets(0, 0, 0, 0), 0, 0));
-		apperancePanel.add(jBackgroundColor, new GridBagConstraints(1, 2, 1, 1,
-				0.0, 0.0, GridBagConstraints.WEST, GridBagConstraints.NONE,
+		apperancePanel.add(jBackgroundColor, new GridBagConstraints(1, 2, 1, 1, 0.0, 0.0, GridBagConstraints.WEST, GridBagConstraints.NONE,
 				new Insets(0, 5, 0, 0), 0, 0));
-		apperancePanel.add(locationPanel, new GridBagConstraints(0, 3, 2, 1,
-				1.0, 0.0, GridBagConstraints.CENTER,
+		apperancePanel.add(locationPanel, new GridBagConstraints(0, 3, 2, 1, 1.0, 0.0, GridBagConstraints.CENTER,
 				GridBagConstraints.HORIZONTAL, new Insets(0, 5, 0, 0), 0, 0));
-		apperancePanel.add(new JPanel(), new GridBagConstraints(0, 4, 2, 1,
-				0.0, 1.0, GridBagConstraints.WEST, GridBagConstraints.BOTH,
+		apperancePanel.add(new JPanel(), new GridBagConstraints(0, 4, 2, 1, 0.0, 1.0, GridBagConstraints.WEST, GridBagConstraints.BOTH,
 				new Insets(0, 0, 0, 0), 0, 0));
 
 		int row = 0;
-		locationPanel.add(progress, new GridBagConstraints(0, row, 1, 1, 0.0,
-				0.0, GridBagConstraints.EAST, GridBagConstraints.NONE,
+		locationPanel.add(progress, new GridBagConstraints(0, row, 1, 1, 0.0, 0.0, GridBagConstraints.EAST, GridBagConstraints.NONE,
 				new Insets(0, 0, 0, 5), 0, 0));
-		locationPanel.add(jProgress, new GridBagConstraints(1, row++, 1, 1,
-				1.0, 0.0, GridBagConstraints.CENTER,
+		locationPanel.add(jProgress, new GridBagConstraints(1, row++, 1, 1, 1.0, 0.0, GridBagConstraints.CENTER,
 				GridBagConstraints.HORIZONTAL, new Insets(0, 0, 0, 0), 0, 0));
-		locationPanel.add(caption, new GridBagConstraints(0, row, 1, 1, 0.0,
-				0.0, GridBagConstraints.EAST, GridBagConstraints.NONE,
+		locationPanel.add(caption, new GridBagConstraints(0, row, 1, 1, 0.0, 0.0, GridBagConstraints.EAST, GridBagConstraints.NONE,
 				new Insets(0, 0, 0, 5), 0, 0));
-		locationPanel.add(jCaption, new GridBagConstraints(1, row++, 1, 1, 1.0,
-				0.0, GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL,
-				new Insets(0, 0, 0, 0), 0, 0));
-		locationPanel.add(extra, new GridBagConstraints(0, row, 1, 1, 0.0, 0.0,
-				GridBagConstraints.EAST, GridBagConstraints.NONE, new Insets(0,
-						0, 0, 5), 0, 0));
-		locationPanel.add(jExtra, new GridBagConstraints(1, row++, 1, 1, 1.0,
-				0.0, GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL,
-				new Insets(0, 0, 0, 0), 0, 0));
-		locationPanel.add(summary, new GridBagConstraints(0, row, 1, 1, 0.0,
-				0.0, GridBagConstraints.EAST, GridBagConstraints.NONE,
-				new Insets(0, 0, 0, 5), 0, 0));
-		locationPanel.add(jSummary, new GridBagConstraints(1, row++, 1, 1, 1.0,
-				0.0, GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL,
-				new Insets(0, 0, 0, 0), 0, 0));
-		locationPanel.add(description, new GridBagConstraints(0, row, 1, 1,
-				0.0, 0.0, GridBagConstraints.EAST, GridBagConstraints.NONE,
-				new Insets(0, 0, 0, 5), 0, 0));
-		locationPanel.add(jDescription, new GridBagConstraints(1, row++, 1, 1,
-				1.0, 0.0, GridBagConstraints.CENTER,
+		locationPanel.add(jCaption, new GridBagConstraints(1, row++, 1, 1, 1.0, 0.0, GridBagConstraints.CENTER,
 				GridBagConstraints.HORIZONTAL, new Insets(0, 0, 0, 0), 0, 0));
-		locationPanel.add(url, new GridBagConstraints(0, row, 1, 1, 0.0, 0.0,
-				GridBagConstraints.EAST, GridBagConstraints.NONE, new Insets(0,
-						0, 0, 5), 0, 0));
-		locationPanel.add(jUrl, new GridBagConstraints(1, row++, 1, 1, 1.0,
-				0.0, GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL,
+		locationPanel.add(extra, new GridBagConstraints(0, row, 1, 1, 0.0, 0.0, GridBagConstraints.EAST, GridBagConstraints.NONE, new Insets(
+				0, 0, 0, 5), 0, 0));
+		locationPanel.add(jExtra, new GridBagConstraints(1, row++, 1, 1, 1.0, 0.0, GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL,
 				new Insets(0, 0, 0, 0), 0, 0));
-		locationPanel.add(album, new GridBagConstraints(0, row, 1, 1, 0.0, 0.0,
-				GridBagConstraints.EAST, GridBagConstraints.NONE, new Insets(0,
-						0, 0, 5), 0, 0));
-		locationPanel.add(jAlbum, new GridBagConstraints(1, row++, 1, 1, 1.0,
-				0.0, GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL,
+		locationPanel.add(summary, new GridBagConstraints(0, row, 1, 1, 0.0, 0.0, GridBagConstraints.EAST, GridBagConstraints.NONE,
+				new Insets(0, 0, 0, 5), 0, 0));
+		locationPanel.add(jSummary, new GridBagConstraints(1, row++, 1, 1, 1.0, 0.0, GridBagConstraints.CENTER,
+				GridBagConstraints.HORIZONTAL, new Insets(0, 0, 0, 0), 0, 0));
+		locationPanel.add(description, new GridBagConstraints(0, row, 1, 1, 0.0, 0.0, GridBagConstraints.EAST, GridBagConstraints.NONE,
+				new Insets(0, 0, 0, 5), 0, 0));
+		locationPanel.add(jDescription, new GridBagConstraints(1, row++, 1, 1, 1.0, 0.0, GridBagConstraints.CENTER,
+				GridBagConstraints.HORIZONTAL, new Insets(0, 0, 0, 0), 0, 0));
+		locationPanel.add(url, new GridBagConstraints(0, row, 1, 1, 0.0, 0.0, GridBagConstraints.EAST, GridBagConstraints.NONE, new Insets(0,
+				0, 0, 5), 0, 0));
+		locationPanel.add(jUrl, new GridBagConstraints(1, row++, 1, 1, 1.0, 0.0, GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL,
+				new Insets(0, 0, 0, 0), 0, 0));
+		locationPanel.add(album, new GridBagConstraints(0, row, 1, 1, 0.0, 0.0, GridBagConstraints.EAST, GridBagConstraints.NONE, new Insets(
+				0, 0, 0, 5), 0, 0));
+		locationPanel.add(jAlbum, new GridBagConstraints(1, row++, 1, 1, 1.0, 0.0, GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL,
 				new Insets(0, 0, 0, 0), 0, 0));
 	}
 
@@ -396,12 +346,23 @@ public class SlideshowPanel extends PreferencePanel implements PreferenceNames {
 			name = GRI18n.getString(MODULE, "locationItem." + id);
 		}
 
+		@Override
 		public String toString() {
 			return name;
 		}
 
+		@Override
 		public boolean equals(Object o) {
 			return o instanceof LocationItem && id == ((LocationItem) o).id;
+		}
+
+		@Override
+		public int hashCode() {
+			final int primer = 37;
+			int result = 1;
+			result = primer * result + id;
+			result = primer * result + (name == null ? 0 : name.hashCode());
+			return result;
 		}
 	}
 }

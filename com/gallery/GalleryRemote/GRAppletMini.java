70b1e188437467603e622250cf8afd5b64678521
witchi <phosco@gmx.de>
2014-07-02 23:35:47 +0200

    ADD: an Override annotation REM: some obsolete casts ADD: a default clause to a switch statement.

--- com/gallery/GalleryRemote/GRAppletMini.java
+++ com/gallery/GalleryRemote/GRAppletMini.java
@@ -49,10 +49,10 @@ import com.gallery.GalleryRemote.util.GRI18n;
 import com.gallery.GalleryRemote.util.ImageUtils;
 
 /**
- * Created by IntelliJ IDEA. User: paour Date: Oct 30, 2003
+ * @author paour
+ * @version Oct 30, 2003
  */
-public class GRAppletMini extends GRApplet implements GalleryRemoteCore,
-		ActionListener, DocumentListener, ListSelectionListener,
+public class GRAppletMini extends GRApplet implements GalleryRemoteCore, ActionListener, DocumentListener, ListSelectionListener,
 		PreferenceNames {
 	private static final long serialVersionUID = 3643620550047079843L;
 
@@ -89,18 +89,13 @@ public class GRAppletMini extends GRApplet implements GalleryRemoteCore,
 	public void initUI() {
 		// update the look and feel
 		SwingUtilities.updateComponentTreeUI(this);
-		int appletFontSize = GalleryRemote.instance().properties.getIntProperty(
-				APPLET_FONTSIZE, 0);
+		int appletFontSize = GalleryRemote.instance().properties.getIntProperty(APPLET_FONTSIZE, 0);
 		if (appletFontSize != 0) {
 			// the default font for many components is too big on Mac and Linux
-			Log.log(Log.LEVEL_TRACE, MODULE, "Overriding font size to "
-					+ appletFontSize);
-			Log.log(Log.LEVEL_TRACE, MODULE, "Default font size "
-					+ UIManager.getFont("TitledBorder.font").getSize());
-			Font f = UIManager.getFont("TitledBorder.font").deriveFont(
-					(float) appletFontSize);
-			Log.log(Log.LEVEL_TRACE, MODULE,
-					"Checking font size " + f.getSize());
+			Log.log(Log.LEVEL_TRACE, MODULE, "Overriding font size to " + appletFontSize);
+			Log.log(Log.LEVEL_TRACE, MODULE, "Default font size " + UIManager.getFont("TitledBorder.font").getSize());
+			Font f = UIManager.getFont("TitledBorder.font").deriveFont((float) appletFontSize);
+			Log.log(Log.LEVEL_TRACE, MODULE, "Checking font size " + f.getSize());
 			UIManager.put("Label.font", f);
 			UIManager.put("TextField.font", f);
 			UIManager.put("Button.font", f);
@@ -133,16 +128,13 @@ public class GRAppletMini extends GRApplet implements GalleryRemoteCore,
 		jbUpdate();
 
 		jPicturesList.setModel(album);
-		jPicturesList.setInputMap(
-				JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT, null);
+		jPicturesList.setInputMap(JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT, null);
 		jPicturesList.setInputMap(JComponent.WHEN_FOCUSED, null);
 		jPicturesList.setInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW, null);
 		jPicturesList.setCellRenderer(new CoreUtils.FileCellRenderer());
 
-		jResize.setSelected(GalleryRemote.instance().properties
-				.getBooleanProperty(RESIZE_BEFORE_UPLOAD));
-		jThumbnails.setSelected(GalleryRemote.instance().properties
-				.getShowThumbnails());
+		jResize.setSelected(GalleryRemote.instance().properties.getBooleanProperty(RESIZE_BEFORE_UPLOAD));
+		jThumbnails.setSelected(GalleryRemote.instance().properties.getShowThumbnails());
 		setShowThumbnails(GalleryRemote.instance().properties.getShowThumbnails());
 
 		jStatusBar.setStatus(GRI18n.getString("MainFrame", "selPicToAdd"));
@@ -153,8 +145,7 @@ public class GRAppletMini extends GRApplet implements GalleryRemoteCore,
 		if (hasStarted && GalleryRemote.instance() != null) {
 			// this is also executed from GRAppletSlideshow
 			if (jDivider != null) {
-				GalleryRemote.instance().properties.setIntProperty(
-						APPLET_DIVIDER_LOCATION, jDivider.getDividerLocation());
+				GalleryRemote.instance().properties.setIntProperty(APPLET_DIVIDER_LOCATION, jDivider.getDividerLocation());
 			}
 
 			ImageUtils.purgeTemp();
@@ -172,6 +163,7 @@ public class GRAppletMini extends GRApplet implements GalleryRemoteCore,
 	public void flushMemory() {
 	}
 
+	@Override
 	public void preloadThumbnails(Iterator<Picture> pictures) {
 		if (thumbnailCache != null) {
 			thumbnailCache.preloadThumbnails(pictures);
@@ -189,8 +181,7 @@ public class GRAppletMini extends GRApplet implements GalleryRemoteCore,
 		if (thumb == null) {
 			thumb = ImageUtils.defaultThumbnail;
 		} else {
-			thumb = ImageUtils.rotateImage(thumb, p.getAngle(), p.isFlipped(),
-					getGlassPane());
+			thumb = ImageUtils.rotateImage(thumb, p.getAngle(), p.isFlipped(), getGlassPane());
 		}
 
 		return thumb;
@@ -227,11 +218,8 @@ public class GRAppletMini extends GRApplet implements GalleryRemoteCore,
 			try {
 				// no update for G2 and for embedded applets (non-embedded
 				// applets are TYPE_STANDALONE)
-				if (!(gallery.getComm(null) instanceof GalleryComm2_5)
-						&& gallery.getType() != Gallery.TYPE_APPLET) {
-					getAppletContext().showDocument(
-							new URL(getCodeBase().toString()
-									+ "add_photos_refresh.php"), "hack");
+				if (!(gallery.getComm(null) instanceof GalleryComm2_5) && gallery.getType() != Gallery.TYPE_APPLET) {
+					getAppletContext().showDocument(new URL(getCodeBase().toString() + "add_photos_refresh.php"), "hack");
 				}
 
 				// use Java to Javascript scripting
@@ -285,8 +273,7 @@ public class GRAppletMini extends GRApplet implements GalleryRemoteCore,
 		jCaption = new JTextArea();
 		jDivider = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT);
 
-		jScrollPane.setBorder(new TitledBorder(BorderFactory
-				.createEmptyBorder(), GRI18n.getString(MODULE, "pictures")));
+		jScrollPane.setBorder(new TitledBorder(BorderFactory.createEmptyBorder(), GRI18n.getString(MODULE, "pictures")));
 
 		jUpload.setText(GRI18n.getString(MODULE, "Upload"));
 		jAdd.setText(GRI18n.getString(MODULE, "Add"));
@@ -298,60 +285,45 @@ public class GRAppletMini extends GRApplet implements GalleryRemoteCore,
 		jButtonPanel.add(jAdd);
 		jButtonPanel.add(jUpload);
 
-		jResize.setToolTipText(GRI18n.getString(MODULE,
-				"ResizeBeforeUploadHelp"));
+		jResize.setToolTipText(GRI18n.getString(MODULE, "ResizeBeforeUploadHelp"));
 		jResize.setText(GRI18n.getString(MODULE, "ResizeBeforeUpload"));
 		jThumbnails.setToolTipText(GRI18n.getString(MODULE, "ThumbnailsHelp"));
 		jThumbnails.setText(GRI18n.getString(MODULE, "Thumbnails"));
-		captionLabel.setText(GRI18n.getString(MODULE, "Caption")
-				+ "            ");
+		captionLabel.setText(GRI18n.getString(MODULE, "Caption") + "            ");
 		jScrollPane.getViewport().add(jPicturesList, null);
 
-		if (!GalleryRemote.instance().properties.getBooleanProperty(
-				APPLET_SHOW_RESIZE, true)) {
+		if (!GalleryRemote.instance().properties.getBooleanProperty(APPLET_SHOW_RESIZE, true)) {
 			jResize.setVisible(false);
 		}
 
 		jCaption.setLineWrap(true);
 		jCaption.setEditable(false);
 		jCaption.setFont(UIManager.getFont("Label.font"));
-		jCaption.setBackground(UIManager
-				.getColor("TextField.inactiveBackground"));
+		jCaption.setBackground(UIManager.getColor("TextField.inactiveBackground"));
 
 		jDivider.setBorder(null);
 		jDivider.setOneTouchExpandable(true);
 		// jDivider.setResizeWeight(.75);
-		jDivider.setDividerLocation(GalleryRemote.instance().properties
-				.getIntProperty(APPLET_DIVIDER_LOCATION));
+		jDivider.setDividerLocation(GalleryRemote.instance().properties.getIntProperty(APPLET_DIVIDER_LOCATION));
 
 		jScrollPane.setMinimumSize(new Dimension(100, 0));
 		jInspector.setMinimumSize(new Dimension(0, 0));
 		jDivider.setLeftComponent(jScrollPane);
 		jDivider.setRightComponent(jInspector);
 
-		jInspector.add(captionLabel, new GridBagConstraints(0, 0, 1, 1, 1.0,
-				0.0, GridBagConstraints.WEST, GridBagConstraints.HORIZONTAL,
+		jInspector.add(captionLabel, new GridBagConstraints(0, 0, 1, 1, 1.0, 0.0, GridBagConstraints.WEST, GridBagConstraints.HORIZONTAL,
 				new Insets(0, 5, 0, 0), 0, 0));
-		jInspector.add(jCaption, new GridBagConstraints(0, 1, 1, 1, 1.0, 1.0,
-				GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(
-						0, 5, 5, 5), 0, 0));
+		jInspector.add(jCaption, new GridBagConstraints(0, 1, 1, 1, 1.0, 1.0, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(
+				0, 5, 5, 5), 0, 0));
 
 		this.getContentPane().setLayout(new GridBagLayout());
-		this.getContentPane().add(
-				jDivider,
-				new GridBagConstraints(0, 0, 1, 1, 1.0, 1.0,
-						GridBagConstraints.CENTER, GridBagConstraints.BOTH,
-						new Insets(0, 0, 0, 0), 0, 0));
-		this.getContentPane().add(
-				jButtonPanel,
-				new GridBagConstraints(0, 1, 1, 1, 0.0, 0.0,
-						GridBagConstraints.CENTER, GridBagConstraints.BOTH,
-						new Insets(5, 5, 5, 5), 0, 0));
+		this.getContentPane().add(jDivider,
+				new GridBagConstraints(0, 0, 1, 1, 1.0, 1.0, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(0, 0, 0, 0), 0, 0));
+		this.getContentPane().add(jButtonPanel,
+				new GridBagConstraints(0, 1, 1, 1, 0.0, 0.0, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(5, 5, 5, 5), 0, 0));
 		this.getContentPane().add(
 				jStatusBar,
-				new GridBagConstraints(0, 2, 1, 1, 1.0, 0.0,
-						GridBagConstraints.CENTER,
-						GridBagConstraints.HORIZONTAL, new Insets(0, 0, 0, 0),
+				new GridBagConstraints(0, 2, 1, 1, 1.0, 0.0, GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL, new Insets(0, 0, 0, 0),
 						0, 0));
 
 		jAdd.addActionListener(this);
@@ -373,10 +345,8 @@ public class GRAppletMini extends GRApplet implements GalleryRemoteCore,
 			// this class is not signed by Gallery, so we can't use the secure
 			// instantiation
 			jsObject = Class.forName("netscape.javascript.JSObject");
-			Method getWindow = jsObject.getMethod("getWindow",
-					new Class<?>[] { Applet.class });
-			call = jsObject.getMethod("call", new Class[] { String.class,
-					Object[].class });
+			Method getWindow = jsObject.getMethod("getWindow", new Class<?>[] { Applet.class });
+			call = jsObject.getMethod("call", new Class[] { String.class, Object[].class });
 			window = getWindow.invoke(null, new Object[] { this });
 		} catch (ClassNotFoundException e) {
 			Log.logException(Log.LEVEL_ERROR, MODULE, e);
@@ -399,7 +369,7 @@ public class GRAppletMini extends GRApplet implements GalleryRemoteCore,
 			int j = 2;
 
 			while (i.hasNext()) {
-				String name = (String) i.next();
+				String name = i.next();
 				JTextArea jExtrafield = new JTextArea();
 				jExtrafield.setName(name);
 				jExtrafields.add(jExtrafield);
@@ -407,16 +377,12 @@ public class GRAppletMini extends GRApplet implements GalleryRemoteCore,
 				jExtrafield.setLineWrap(true);
 				jExtrafield.setEditable(false);
 				jExtrafield.setFont(UIManager.getFont("Label.font"));
-				jExtrafield.setBackground(UIManager
-						.getColor("TextField.inactiveBackground"));
+				jExtrafield.setBackground(UIManager.getColor("TextField.inactiveBackground"));
 
-				jInspector.add(new JLabel(name + ":"), new GridBagConstraints(
-						0, j++, 1, 1, 1.0, 0.0, GridBagConstraints.WEST,
-						GridBagConstraints.HORIZONTAL, new Insets(0, 5, 0, 0),
-						0, 0));
-				jInspector.add(jExtrafield, new GridBagConstraints(0, j++, 1,
-						1, 1.0, 1.0, GridBagConstraints.CENTER,
-						GridBagConstraints.BOTH, new Insets(0, 5, 5, 5), 0, 0));
+				jInspector.add(new JLabel(name + ":"), new GridBagConstraints(0, j++, 1, 1, 1.0, 0.0, GridBagConstraints.WEST,
+						GridBagConstraints.HORIZONTAL, new Insets(0, 5, 0, 0), 0, 0));
+				jInspector.add(jExtrafield, new GridBagConstraints(0, j++, 1, 1, 1.0, 1.0, GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+						new Insets(0, 5, 5, 5), 0, 0));
 
 				jExtrafield.getDocument().addDocumentListener(this);
 			}
@@ -445,6 +411,8 @@ public class GRAppletMini extends GRApplet implements GalleryRemoteCore,
 			case KeyEvent.VK_DOWN:
 				CoreUtils.selectNextPicture();
 				break;
+			default:
+				break;
 			}
 		}
 	}
@@ -462,17 +430,14 @@ public class GRAppletMini extends GRApplet implements GalleryRemoteCore,
 			}
 		} else if (source == jUpload) {
 			g2Feedback("startingUpload", new Object[] {});
-			gallery.doUploadFiles(new UploadProgress(DialogUtil
-					.findParentWindow(this)) {
+			gallery.doUploadFiles(new UploadProgress(DialogUtil.findParentWindow(this)) {
 				@Override
 				public void doneUploading(String newItemName, Picture picture) {
-					g2Feedback("uploadedOne", new Object[] { newItemName,
-							picture.toString() });
+					g2Feedback("uploadedOne", new Object[] { newItemName, picture.toString() });
 				}
 			});
 		} else if (source == jResize) {
-			GalleryRemote.instance().properties.setBooleanProperty(
-					RESIZE_BEFORE_UPLOAD, jResize.isSelected());
+			GalleryRemote.instance().properties.setBooleanProperty(RESIZE_BEFORE_UPLOAD, jResize.isSelected());
 		} else if (source == jThumbnails) {
 			setShowThumbnails(jThumbnails.isSelected());
 		}
@@ -494,7 +459,7 @@ public class GRAppletMini extends GRApplet implements GalleryRemoteCore,
 	}
 
 	public void textUpdate(DocumentEvent e) {
-		Picture p = (Picture) jPicturesList.getSelectedValue();
+		Picture p = jPicturesList.getSelectedValue();
 
 		if (p != null) {
 			if (e.getDocument() == jCaption.getDocument()) {
@@ -502,11 +467,10 @@ public class GRAppletMini extends GRApplet implements GalleryRemoteCore,
 			} else {
 				Iterator<JTextArea> i = jExtrafields.iterator();
 				while (i.hasNext()) {
-					JTextArea jExtrafield = (JTextArea) i.next();
+					JTextArea jExtrafield = i.next();
 
 					if (e.getDocument() == jExtrafield.getDocument()) {
-						p.setExtraField(jExtrafield.getName(),
-								jExtrafield.getText());
+						p.setExtraField(jExtrafield.getName(), jExtrafield.getText());
 						// Log.log("textUpdate: " + jExtrafield.getName() +
 						// " - " + jExtrafield.getText());
 					}
@@ -517,22 +481,20 @@ public class GRAppletMini extends GRApplet implements GalleryRemoteCore,
 
 	@Override
 	public void valueChanged(ListSelectionEvent e) {
-		Picture p = (Picture) jPicturesList.getSelectedValue();
+		Picture p = jPicturesList.getSelectedValue();
 
 		if (p == null) {
 			jCaption.setText("");
 			jCaption.setEditable(false);
-			jCaption.setBackground(UIManager
-					.getColor("TextField.inactiveBackground"));
+			jCaption.setBackground(UIManager.getColor("TextField.inactiveBackground"));
 
 			Iterator<JTextArea> i = jExtrafields.iterator();
 			while (i.hasNext()) {
-				JTextArea jExtrafield = (JTextArea) i.next();
+				JTextArea jExtrafield = i.next();
 
 				jExtrafield.setText("");
 				jExtrafield.setEditable(false);
-				jExtrafield.setBackground(UIManager
-						.getColor("TextField.inactiveBackground"));
+				jExtrafield.setBackground(UIManager.getColor("TextField.inactiveBackground"));
 			}
 		} else {
 			jCaption.setText(p.getCaption());
@@ -541,15 +503,14 @@ public class GRAppletMini extends GRApplet implements GalleryRemoteCore,
 
 			Iterator<JTextArea> i = jExtrafields.iterator();
 			while (i.hasNext()) {
-				JTextArea jExtrafield = (JTextArea) i.next();
+				JTextArea jExtrafield = i.next();
 
 				// Log.log("valueChanged: " + jExtrafield.getName() + " - " +
 				// jExtrafield.getText());
 				String extrafield = p.getExtraField(jExtrafield.getName());
 				jExtrafield.setText(extrafield == null ? "" : extrafield);
 				jExtrafield.setEditable(true);
-				jExtrafield.setBackground(UIManager
-						.getColor("TextField.background"));
+				jExtrafield.setBackground(UIManager.getColor("TextField.background"));
 			}
 		}
 	}
@@ -562,8 +523,7 @@ public class GRAppletMini extends GRApplet implements GalleryRemoteCore,
 		if (show) {
 			preloadThumbnails(album.getPictures());
 
-			jPicturesList.setFixedCellHeight(GalleryRemote.instance().properties
-					.getThumbnailSize().height + 4);
+			jPicturesList.setFixedCellHeight(GalleryRemote.instance().properties.getThumbnailSize().height + 4);
 		} else {
 			if (thumbnailCache != null) {
 				thumbnailCache.cancelLoad();
@@ -577,9 +537,7 @@ public class GRAppletMini extends GRApplet implements GalleryRemoteCore,
 	public void g2Feedback(String method, Object[] params) {
 		if (gallery.getGalleryVersion() == 2) {
 			try {
-				Log.log(Log.LEVEL_TRACE, MODULE, "Invoking Javascript method '"
-						+ method + "' with " + Arrays.asList(params) + " on "
-						+ window);
+				Log.log(Log.LEVEL_TRACE, MODULE, "Invoking Javascript method '" + method + "' with " + Arrays.asList(params) + " on " + window);
 				call.invoke(window, new Object[] { method, params });
 			} catch (Throwable e) {
 				Log.logException(Log.LEVEL_ERROR, MODULE, e);

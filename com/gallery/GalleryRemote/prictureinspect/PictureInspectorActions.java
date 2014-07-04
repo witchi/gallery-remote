0df4a6d6de7d81ea7ff7a238de084baeeff5506e
André Rothe <phosco@gmx.de>
2014-07-04 19:18:10 +0200

    ADD: some Actions, we should change that to an enum.

--- com/gallery/GalleryRemote/prictureinspect/PictureInspectorActions.java
+++ com/gallery/GalleryRemote/prictureinspect/PictureInspectorActions.java
@@ -2,8 +2,13 @@ package com.gallery.GalleryRemote.prictureinspect;
 
 public interface PictureInspectorActions {
 
+	static final String ACTION_ROTATE_LEFT = "Left";
+	static final String ACTION_ROTATE_RIGHT = "Right";
+	static final String ACTION_FLIP = "Flip";
 	static final String ACTION_DELETE = "Delete";
 	static final String ACTION_UP = "Up";
 	static final String ACTION_DOWN = "Down";
-
+	static final String ACTION_REFRESH = "Refresh";
+	static final String ACTION_REMOVE_EXTRA_FIELDS = "RemoveExtraFields";
+	static final String ACTION_SET_EXTRA_FIELDS = "SetExtraFields";
 }

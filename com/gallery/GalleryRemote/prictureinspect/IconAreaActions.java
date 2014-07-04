4d5279f3fb06ec4bc06b2e643c90bff9e87fb36d
witchi <phosco@gmx.de>
2014-07-04 12:06:34 +0200

    ADD: two interfaces, which define the Action names for buttons/menus

---
+++ com/gallery/GalleryRemote/prictureinspect/IconAreaActions.java
@@ -0,0 +1,9 @@
+package com.gallery.GalleryRemote.prictureinspect;
+
+public interface IconAreaActions {
+
+	static String ACTION_ROTATE_LEFT = "Left";
+	static String ACTION_ROTATE_RIGHT = "Right";
+	static String ACTION_FLIP = "Flip";
+	
+}
---
+++ com/gallery/GalleryRemote/prictureinspect/PictureInspectorActions.java
@@ -0,0 +1,9 @@
+package com.gallery.GalleryRemote.prictureinspect;
+
+public interface PictureInspectorActions {
+
+	static final String ACTION_DELETE = "Delete";
+	static final String ACTION_UP = "Up";
+	static final String ACTION_DOWN = "Down";
+
+}

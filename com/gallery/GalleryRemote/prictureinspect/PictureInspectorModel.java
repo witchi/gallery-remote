90b349792ab8990610743e4d0ebc23cc0c5d719f
witchi <phosco@gmx.de>
2014-07-05 02:22:53 +0200

    REM: some functions to calculate the filesize CHG: the method textUpdate has been renamed into documentUpdate, it does not longer user events

--- com/gallery/GalleryRemote/prictureinspect/PictureInspectorModel.java
+++ com/gallery/GalleryRemote/prictureinspect/PictureInspectorModel.java
@@ -2,20 +2,17 @@ package com.gallery.GalleryRemote.prictureinspect;
 
 import java.awt.Image;
 import java.awt.event.ActionEvent;
-import java.text.NumberFormat;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import javax.swing.event.DocumentEvent;
 import javax.swing.text.BadLocationException;
 import javax.swing.text.Document;
 import javax.swing.text.PlainDocument;
 
 import com.gallery.GalleryRemote.MainFrame;
-import com.gallery.GalleryRemote.model.Album;
 import com.gallery.GalleryRemote.model.Picture;
 import com.gallery.GalleryRemote.util.AbstractModel;
 
@@ -93,28 +90,29 @@ public class PictureInspectorModel extends AbstractModel {
 		}
 	}
 
-	void textUpdate(DocumentEvent e) {
-
+	void documentUpdate(Document doc) {
 		if (pictureList == null || pictureList.size() != 1) {
 			return;
 		}
 
 		Picture p = pictureList.get(0);
 
-		if (e.getDocument() == captionDocument) {
+		if (doc == captionDocument) {
 			p.setCaption(getDocumentText(captionDocument));
 			return;
 		}
 
 		for (String name : extraFieldDocuments.keySet()) {
-			if (e.getDocument() == extraFieldDocuments.get(name)) {
-				if (e.getDocument().getLength() == 0) {
-					p.removeExtraField(name);
-				} else {
-					p.setExtraField(name, getDocumentText(e.getDocument()));
-				}
-				break; // extra field was found
+			if (doc != extraFieldDocuments.get(name)) {
+				continue;
 			}
+				
+			if (doc.getLength() == 0) {
+				p.removeExtraField(name);
+			} else {
+				p.setExtraField(name, getDocumentText(doc));
+			}
+			break; // extra field was found
 		}
 	}
 
@@ -130,8 +128,7 @@ public class PictureInspectorModel extends AbstractModel {
 		notifyListeners(new ActionEvent(this, 0, PictureInspectorActions.ACTION_REMOVE_EXTRA_FIELDS));
 	}
 
-	private void fireSetExtraFieldsEvent(Map<String, Document> docList) {
-		// TODO: we must transport the doclist or provide a getter method
+	private void fireSetExtraFieldsEvent() {
 		notifyListeners(new ActionEvent(this, 0, PictureInspectorActions.ACTION_SET_EXTRA_FIELDS));
 	}
 
@@ -165,25 +162,14 @@ public class PictureInspectorModel extends AbstractModel {
 			}
 			extraFieldDocuments.put(name, d);
 		}
-		fireSetExtraFieldsEvent(Collections.unmodifiableMap(extraFieldDocuments));
+		fireSetExtraFieldsEvent();
+	}
 
+	Map<String, Document> getExtraFields() {
+		return Collections.unmodifiableMap(extraFieldDocuments);
 	}
 
 	boolean hasCapability(Picture p, int capability) {
 		return p.getParentAlbum().getGallery().getComm(mainFrame.jStatusBar).hasCapability(mainFrame.jStatusBar, capability);
-	}
-
-	String getFileSize(Picture p) {
-		if (p == null) {
-			return "0";
-		}
-		return NumberFormat.getInstance().format((int) p.getFileSize());
-	}
-
-	String getFileSize(List<Picture> pList) {
-		if (pList == null) {
-			return "0";
-		}
-		return NumberFormat.getInstance().format(Album.getObjectFileSize(pList));
 	}
 }

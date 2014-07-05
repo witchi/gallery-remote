0c748f53cd60af7e2e74b8d121b4078dac3e9af2
witchi <phosco@gmx.de>
2014-07-05 02:21:08 +0200

    ADD: new methods called by the controller

--- com/gallery/GalleryRemote/prictureinspect/PictureInspector.java
+++ com/gallery/GalleryRemote/prictureinspect/PictureInspector.java
@@ -9,8 +9,6 @@ import javax.swing.text.Document;
 public interface PictureInspector {
 
 	// called by view
-	void refresh();
-
 	Collection<PictureFieldTextArea> setExtraFields(Map<String, Document> docList);
 
 	void removeExtraFields();
@@ -28,5 +26,11 @@ public interface PictureInspector {
 	AbstractButton getFlipButton();
 	
 	PictureFieldTextArea getCaption(); 
+	
+	// called by controller
+	void setEnabled(boolean enabled);
+	void refreshWithOnePicture(PictureInspectorDTO dto);
+	void refreshWithoutPicture();
+	void refreshWithMultiplePictures(PictureInspectorDTO dto);
 	
 }

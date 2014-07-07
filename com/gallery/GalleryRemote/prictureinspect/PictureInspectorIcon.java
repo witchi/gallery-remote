45016967a864fdbb157646d849c895221643e5e2
André Rothe <phosco@gmx.de>
2014-07-07 12:19:50 +0200

    CHG: the constructor of the JLabel uses setText(), but this results in a NullPointerException because the Document has not been set at this point.

--- com/gallery/GalleryRemote/prictureinspect/PictureInspectorIcon.java
+++ com/gallery/GalleryRemote/prictureinspect/PictureInspectorIcon.java
@@ -25,7 +25,7 @@ public class PictureInspectorIcon extends JLabel implements DocumentListener {
 	public String getText() {
 		try {
 			return document.getText(0, document.getLength());
-		} catch (BadLocationException e) {
+		} catch (Exception e) {
 			return "";
 		}
 	}
@@ -36,7 +36,7 @@ public class PictureInspectorIcon extends JLabel implements DocumentListener {
 		try {
 			document.remove(0, document.getLength());
 			document.insertString(0, text, null);
-		} catch (BadLocationException e) {
+		} catch (Exception e) {
 			// do nothing
 		}
 	}

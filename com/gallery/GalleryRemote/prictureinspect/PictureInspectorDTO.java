a5e3aeb2c3fff2ecc05e6dc222f382a46c8034c2
André Rothe <phosco@gmx.de>
2014-07-07 10:23:21 +0200

    CHG: the DTO has been simplified, we transfer only displayable data

--- com/gallery/GalleryRemote/prictureinspect/PictureInspectorDTO.java
+++ com/gallery/GalleryRemote/prictureinspect/PictureInspectorDTO.java
@@ -1,18 +1,32 @@
 package com.gallery.GalleryRemote.prictureinspect;
 
+import java.awt.Dimension;
 import java.awt.Image;
-
-import com.gallery.GalleryRemote.model.Picture;
 
 class PictureInspectorDTO {
 
-	private Picture picture;
 	private Image thumbnail;
-	private int pictureListSize;
-	private int fileSize;
 	private boolean hasCapability;
+	private Dimension iconSize;
+	private boolean viewEnabled;
 
 	PictureInspectorDTO() {
+	}
+
+	public boolean isViewEnabled() {
+		return viewEnabled;
+	}
+
+	public void setViewEnabled(boolean viewEnabled) {
+		this.viewEnabled = viewEnabled;
+	}
+
+	public Dimension getIconSize() {
+		return iconSize;
+	}
+
+	public void setIconSize(Dimension iconSize) {
+		this.iconSize = iconSize;
 	}
 
 	boolean hasCapability() {
@@ -23,35 +37,11 @@ class PictureInspectorDTO {
 		this.hasCapability = hasCapability;
 	}
 
-	void setPicture(Picture p) {
-		this.picture = p;
-	}
-
 	void setThumbnail(Image thumbnail) {
 		this.thumbnail = thumbnail;
 	}
 
-	void setPictureListSize(int size) {
-		this.pictureListSize = size;
-	}
-
-	void setFileSize(int size) {
-		this.fileSize = size;
-	}
-
-	Picture getPicture() {
-		return picture;
-	}
-
 	Image getThumbnail() {
 		return thumbnail;
-	}
-
-	int getPictureListSize() {
-		return pictureListSize;
-	}
-
-	int getFileSize() {
-		return fileSize;
 	}
 }

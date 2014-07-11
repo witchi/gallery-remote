18cf13d0fd90dc5e1b168b1b50e002770ea962b3
André Rothe <phosco@gmx.de>
2014-07-11 12:25:55 +0200

    ADD: a combobox implementation which provides some convenience methods, it hides also a lot of functionality by a small interface

---
+++ com/gallery/GalleryRemote/albuminspector/AlbumFieldComboBoxImpl.java
@@ -0,0 +1,43 @@
+package com.gallery.GalleryRemote.albuminspector;
+
+import java.awt.event.KeyListener;
+import java.util.Vector;
+
+import javax.swing.DefaultListCellRenderer;
+import javax.swing.JComboBox;
+
+import com.gallery.GalleryRemote.prefs.UploadPanel.ResizeSize;
+
+public class AlbumFieldComboBoxImpl extends JComboBox<ResizeSize> implements AlbumFieldComboBox {
+
+	private static final long serialVersionUID = -4594464883786453342L;
+
+	public AlbumFieldComboBoxImpl(Vector<ResizeSize> defaultSizes) {
+		super(defaultSizes);
+	}
+
+	@Override
+	public String getEditedItem() {
+		return getEditor().getItem().toString();
+	}
+
+	@Override
+	public String getSelectedItemAsString() {
+		return getSelectedItem().toString();
+	}
+
+	@Override
+	public void addKeyboardListener(KeyListener listener) {
+		getEditor().getEditorComponent().addKeyListener(listener);
+	}
+
+	@Override
+	public void setSelectedItem(ResizeSize s) {
+		super.setSelectedItem(s);
+	}
+
+	@Override
+	public void setRenderer(DefaultListCellRenderer aRenderer) {
+		super.setRenderer(aRenderer);
+	}
+}

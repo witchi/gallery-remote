0072fddd6a211ad602c443dc09f59ce24f59b9fc
witchi <phosco@gmx.de>
2014-07-14 23:13:01 +0200

    ADD: an info() method.

--- src/com/gallery/galleryremote/util/log/Logger.java
+++ src/com/gallery/galleryremote/util/log/Logger.java
@@ -10,6 +10,10 @@ public class Logger {
 		this.logger = logger;
 	}
 
+	public static Logger getLogger(Class<?> clazz) {
+		return new Logger(java.util.logging.Logger.getLogger(clazz.getName()));
+	}
+
 	public void fine(StackTraceElement[] stack) {
 		String message = "";
 		for (StackTraceElement elem : stack) {
@@ -18,22 +22,20 @@ public class Logger {
 		fine(message);
 	}
 
-	public static Logger getLogger(Class<?> clazz) {
-		return new Logger(java.util.logging.Logger.getLogger(clazz.getName()));
+	public void fine(String msg) {
+		logger.fine(msg);
 	}
 
-	public void fine(String string) {
-		logger.fine(string);
-	}
-
-	public void severe(String string) {
-		logger.severe(string);
+	public void severe(String msg) {
+		logger.severe(msg);
 	}
 
 	public void throwing(Throwable thrown) {
 		logger.log(Level.FINE, thrown.getMessage(), thrown);
 	}
 
-	
+	public void info(String msg) {
+		logger.info(msg);
+	}
 	
 }

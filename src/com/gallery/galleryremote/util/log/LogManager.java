eca6efc2212e945493a78c3c64f1f69f019539dd
witchi <phosco@gmx.de>
2014-07-14 23:01:45 +0200

    ADD: the new LogManager class initializes the Logging framework.

---
+++ src/com/gallery/galleryremote/util/log/LogManager.java
@@ -0,0 +1,52 @@
+package com.gallery.galleryremote.util.log;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.FileHandler;
+import java.util.logging.Formatter;
+import java.util.logging.Handler;
+import java.util.logging.Level;
+
+public class LogManager {
+
+	public static void setup(int level, boolean toConsole) throws IOException {
+
+		java.util.logging.Logger logger = java.util.logging.Logger.getLogger(java.util.logging.Logger.GLOBAL_LOGGER_NAME);
+		logger.setUseParentHandlers(true);
+
+		if (!toConsole) {
+			java.util.logging.Logger rootLogger = java.util.logging.Logger.getLogger("");
+			Handler[] handlers = rootLogger.getHandlers();
+
+			if (handlers[0] instanceof ConsoleHandler) {
+				rootLogger.removeHandler(handlers[0]);
+			}
+		}
+
+		// map the levels from the GalleryRemote properties file
+		// TODO: remove that, if we have only Java-Logging values.
+		switch (level) {
+		case 0:
+			logger.setLevel(Level.SEVERE);
+			break;
+		case 1:
+			logger.setLevel(Level.WARNING);
+			break;
+		case 2:
+			logger.setLevel(Level.INFO);
+			break;
+		case 3:
+			logger.setLevel(Level.FINE);
+			break;
+		default:
+			logger.setLevel(Level.parse("" + level));
+		}
+
+		FileHandler fh = new FileHandler(new File(System.getProperty("java.io.tmpdir"), "GalleryRemoteLog2.txt").getPath());
+		Formatter formatter = new GalleryLogFormatter();
+		fh.setFormatter(formatter);
+		fh.setLevel(Level.ALL);
+		logger.addHandler(fh);
+	}
+}
--- src/com/gallery/galleryremote/util/log/Logger.java
+++ src/com/gallery/galleryremote/util/log/Logger.java
@@ -1,11 +1,5 @@
 package com.gallery.galleryremote.util.log;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.logging.ConsoleHandler;
-import java.util.logging.FileHandler;
-import java.util.logging.Formatter;
-import java.util.logging.Handler;
 import java.util.logging.Level;
 
 public class Logger {
@@ -35,46 +29,11 @@ public class Logger {
 	public void severe(String string) {
 		logger.severe(string);
 	}
-	
+
 	public void throwing(Throwable thrown) {
 		logger.log(Level.FINE, thrown.getMessage(), thrown);
 	}
+
 	
-	public static void setup(int level, boolean toConsole) throws IOException {
-
-		java.util.logging.Logger logger = java.util.logging.Logger.getLogger(java.util.logging.Logger.GLOBAL_LOGGER_NAME);
-
-		if (!toConsole) {
-			java.util.logging.Logger rootLogger = java.util.logging.Logger.getLogger("");
-			Handler[] handlers = rootLogger.getHandlers();
-
-			if (handlers[0] instanceof ConsoleHandler) {
-				rootLogger.removeHandler(handlers[0]);
-			}
-		}
-
-		// map the levels from the GalleryRemote properties file
-		// TODO: remove that, if we have only Java-Logging values.
-		switch (level) {
-		case 0:
-			logger.setLevel(Level.SEVERE);
-			break;
-		case 1:
-			logger.setLevel(Level.WARNING);
-			break;
-		case 2:
-			logger.setLevel(Level.INFO);
-			break;
-		case 3:
-			logger.setLevel(Level.FINE);
-			break;
-		default:
-			logger.setLevel(Level.parse("" + level));
-		}
-
-		FileHandler fileTxt = new FileHandler(new File(System.getProperty("java.io.tmpdir"), "GalleryRemoteLog2.txt").getPath());
-		Formatter formatter = new GalleryLogFormatter();
-		fileTxt.setFormatter(formatter);
-		logger.addHandler(fileTxt);
-	}
+	
 }

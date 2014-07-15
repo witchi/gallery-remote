8f58614c33db5f1b7b580a56736f700260dc6632
André Rothe <phosco@gmx.de>
2014-07-15 14:56:20 +0200

    CHG: the code NONE has been removed, so we can use the ordinal() method instead of our code() method.

--- src/com/gallery/galleryremote/statusbar/StatusLevel.java
+++ src/com/gallery/galleryremote/statusbar/StatusLevel.java
@@ -1,15 +1,5 @@
 package com.gallery.galleryremote.statusbar;
 
 public enum StatusLevel {
-	NONE(-1), GENERIC(0), BACKGROUND(1), CACHE(2), UPLOAD_ONE(3), UPLOAD_ALL(4), UNINTERRUPTIBLE(5);
-	
-	private int code;
-	
-	private StatusLevel(int code) {
-		this.code = code;
-	}
-	
-	public int code() {
-		return code;
-	}
+	GENERIC, BACKGROUND, CACHE, UPLOAD_ONE, UPLOAD_ALL, UNINTERRUPTIBLE;
 }

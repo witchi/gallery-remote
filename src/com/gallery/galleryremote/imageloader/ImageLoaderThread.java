4ac057aa24a1b19bc2aa326a6bb7024c6856f25b
André Rothe <phosco@gmx.de>
2014-08-01 15:11:08 +0200

    CHG: the ImageLoaderThread has been re-implemented, it is now threadsafe ADD: we use a little work unit class inside the thread to hold all information about a picture.

--- src/com/gallery/galleryremote/imageloader/ImageLoaderThread.java
+++ src/com/gallery/galleryremote/imageloader/ImageLoaderThread.java
@@ -1,61 +1,104 @@
 package com.gallery.galleryremote.imageloader;
 
-import java.awt.Image;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
-import com.gallery.galleryremote.Log;
 import com.gallery.galleryremote.model.Picture;
+import com.gallery.galleryremote.util.ImageProcessor;
 import com.gallery.galleryremote.util.log.Logger;
 
-public class ImageLoaderThread implements Runnable {
-	
+/**
+ * This thread runs always, till someone calls the kill() method. It gets some
+ * ImageLoaderWorkUnit instances from its workList. If the list is empty, the
+ * thread will check the content of the list every 150 ms. If there is no new
+ * work, the thread will sleep.
+ * 
+ * @author arothe
+ * 
+ */
+public class ImageLoaderThread extends Thread {
+
 	private static final Logger LOGGER = Logger.getLogger(ImageLoaderThread.class);
-	
-	private Picture picture;
-	private boolean stillRunning = false;
-	private boolean notify = false;
+
+	private final List<ImageLoaderWorkUnit> workList;
+	private volatile boolean killed;
+	private final ImageProcessor processor;
+
+	public ImageLoaderThread(ImageProcessor processor) {
+		LOGGER.fine("Creating class instance...");
+
+		this.processor = processor;
+		this.workList = Collections.synchronizedList(new ArrayList<ImageLoaderWorkUnit>());
+		this.killed = false;
+		start();
+	}
 
 	@Override
 	public void run() {
-		LOGGER.fine("Start loading image " + picture);
-		
-		Picture tmpPicture = null;
-		Image tmpImage = null;
-		while (picture != null) {
-			synchronized (this) {
-				tmpPicture = picture;
-				picture = null;
+		LOGGER.fine("Starting image loading thread");
+
+		while (!Thread.currentThread().isInterrupted()) {
+
+			ImageLoaderWorkUnit unit = getLastWorkUnit();
+
+			if (unit == null) {
+				hibernate(unit);
+				continue;
 			}
 
-			tmpImage = getSizedIconForce(tmpPicture);
+			unit.setImage(processor.getSizedIconForce(unit.getPicture()));
 
-			if (tmpImage == null) {
-				notify = false;
+			if (unit == getLastWorkUnit()) {
+				firePictureReady(unit);
+				flushWorkList(unit);
+				hibernate(unit);
 			}
 		}
+		LOGGER.fine("Ending image loading thread");
+	}
 
-		stillRunning = false;
-
-		if (notify) {
-			pictureReady(tmpImage, tmpPicture);
-			notify = false;
+	private void flushWorkList(ImageLoaderWorkUnit unit) {
+		if (unit == null) {
+			return;
 		}
+		int idx = workList.indexOf(unit);
+		for (int i = 0; i < idx; i++) {
+			workList.remove(i);
+		}
+	}
 
-		LOGGER.fine("Ending image loading");
+	private ImageLoaderWorkUnit getLastWorkUnit() {
+		return workList.isEmpty() ? null : workList.get(workList.size() - 1);
+	}
+
+	private void hibernate(ImageLoaderWorkUnit unit) {
+		try {
+			while (unit == getLastWorkUnit()) {
+				Thread.sleep(150);
+			}
+		} catch (InterruptedException ex) {
+			if (killed) {
+				Thread.currentThread().interrupt();
+			}
+		}
+	}
+
+	private void firePictureReady(ImageLoaderWorkUnit unit) {
+		if (!unit.isNotification() || unit.getImage() == null) {
+			return;
+		}
+		processor.pictureReady(unit.getImage(), unit.getPicture());
+	}
+
+	public void kill() {
+		LOGGER.fine("Killing image loading thread");
+		this.killed = true;
+		interrupt();
 	}
 
 	public void loadPicture(Picture picture, boolean notify) {
-		LOGGER.fine("loadPicture " + picture);
-
-		this.picture = picture;
-
-		if (notify) {
-			this.notify = true;
-		}
-
-		if (!stillRunning) {
-			stillRunning = true;
-			LOGGER.fine("Calling Start for load picture");
-			new Thread(this).start();
-		}
+		LOGGER.fine("load picture " + picture);
+		workList.add(new ImageLoaderWorkUnit(picture, notify));
 	}
 }
